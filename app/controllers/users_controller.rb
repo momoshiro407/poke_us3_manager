2da28a73510e55a4b8972eeb4e48dd2b7479aa9f
momoshiro <rozabmmsr@gmail.com>
2018-09-23 23:11:40 +0900

    初期userページ設定+DB設定 (#2)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.find(1)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :top_page
+  resources :users
 end
