baecc28f183367afe97a1f54307cbc38b609506c
momoshiro407 <rozabmmsr@gmail.com>
2018-09-30 23:17:49 +0900

    ユーザー更新機能+認可機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   attr_accessor :remember_token
 
   def new
@@ -26,6 +29,13 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'プロフィールが更新されました'
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
@@ -37,6 +47,18 @@ class UsersController < ApplicationController
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'ログインしてください'
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
