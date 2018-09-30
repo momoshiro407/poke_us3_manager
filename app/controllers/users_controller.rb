01f8ddb6300c8967ed84a5a94748ae0dcc64c19d
momoshiro407 <rozabmmsr@gmail.com>
2018-09-30 19:06:58 +0900

    full_title追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  end
+
   private
 
   def user_params
