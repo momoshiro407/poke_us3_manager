6963d2366cd646d46f4f947718a87b33fd520971
momoshiro <rozabmmsr@gmail.com>
2018-10-13 17:53:31 +0900

    モンスター登録機能実装 (#12)
    * Aboutページ修正
    
    * モンスター登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token
 
   has_many :species_groups
+  has_many :monsters, through: :species_groups
 
   validates :name, presence: true, length: { maximum: 100 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
